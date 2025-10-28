d861134f1834faac5f9ed886bd3283dc19dbc0b7
handiman <handiman@users.noreply.github.com>
2025-10-28 19:33:37 +0100

    :moyai:

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,6 +4,13 @@ export const config: Config = {
   namespace: 'cvchat',
   outputTargets: [
     {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'dist-custom-elements'
+    },
+    {
       type: 'www',
       serviceWorker: null,
       copy: [
